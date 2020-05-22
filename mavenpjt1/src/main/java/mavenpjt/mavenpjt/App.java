2ff1076eff594664a03441d7365fd2a7b2f01e41
arti-t <itrapurohit@yahoo.co.in>
2020-05-22 06:46:04 +0530

    This is first commit.
    uploading files.

---
+++ mavenpjt1/src/main/java/mavenpjt/mavenpjt/App.java
@@ -0,0 +1,15 @@
+package mavenpjt.mavenpjt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int i=10;
+        System.out.println(i);
+    }
+}
